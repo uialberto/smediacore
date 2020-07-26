2f69df6348aeda41006a13f6e6b77503708abf90
Alberto Baigorria <uialberto@gmail.com>
2020-07-26 07:59:36 -0400

    RESTFul API with .NET Core 3.1 - Part 6 - Data Transfer Objects and Automapper

--- CoreSolutionLab/Smedia.WebApi/Controllers/PostController.cs
+++ CoreSolutionLab/Smedia.WebApi/Controllers/PostController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Uibasoft.Smedia.Core.DTOs;
 using Uibasoft.Smedia.Core.Entities;
 using Uibasoft.Smedia.Core.Interfaces;
 
@@ -11,25 +16,53 @@ namespace Smedia.WebApi.Controllers
     public class PostController : ControllerBase
     {
         private readonly IRepoPost _repoPost;
-        public PostController(IRepoPost repoPost)
+        private readonly IMapper _mapper;
+        public PostController(IRepoPost repoPost, IMapper pMapper)
         {
             _repoPost = repoPost ?? throw new ArgumentNullException(nameof(repoPost));
+            _mapper = pMapper ?? throw new ArgumentNullException(nameof(pMapper));
         }
         [HttpGet]
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _repoPost.GetPosts();
-            return Ok(posts);
+            var postsDtos = _mapper.Map<IEnumerable<PostDto>>(posts);
+            //var postsDtos = posts.Select(ele => new PostDto()
+            //{
+            //    PostId = ele.PostId,
+            //    Date = ele.Date,
+            //    Description = ele.Description,
+            //    Image = ele.Image,
+            //    UserId = ele.UserId
+            //});
+            return Ok(postsDtos);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _repoPost.GetPost(id);
-            return Ok(post);
+            var postDto = _mapper.Map<PostDto>(post);
+            //var postDto = new PostDto()
+            //{
+            //    PostId = post.PostId,
+            //    Date = post.Date,
+            //    Description = post.Description,
+            //    Image = post.Image,
+            //    UserId = post.UserId
+            //};
+            return Ok(postDto);
         }
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            //var post = new Post()
+            //{                
+            //    Date = postDto.Date,
+            //    Description = postDto.Description,
+            //    Image = postDto.Image,
+            //    UserId = postDto.UserId
+            //};
+            var post = _mapper.Map<Post>(postDto);
             await _repoPost.Insert(post);
             return Ok(post);
         }
---
+++ CoreSolutionLab/Uibasoft.Smedia.DataAccess/Helpers/BasicProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Uibasoft.Smedia.Core.DTOs;
+using Uibasoft.Smedia.Core.Entities;
+
+namespace Uibasoft.Smedia.DataAccess.Helpers
+{
+    public class BasicProfile : Profile
+    {
+        public BasicProfile()
+        {
+            CreateMap<Post, PostDto>();
+            CreateMap<PostDto, Post>();
+        }
+    }
+}
